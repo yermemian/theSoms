733b1e08bad2fd51986757376d733328402c677a
zoechristenson <zchristenson@luc.edu>
2020-04-18 13:57:10 -0500

    I added the main class

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public static void main {
+
+    //enter name of town
+    Scanner input = new Scanner (System.in);
+    System.out.print("Input what you would like to name your town: ")
+    String townName = input.next();
+    System.out.println("The name of the town is" + townName);
+
+    //starting population
+    int populationSize = 100;
+
+    while (born > die){
+    populationSize=populationSize+1;
+    return populationSize;
+    }
+    //add new kids to the population  = born
+    //take away numbers if people die = die
+
+    while (populationSize > 0){
+    //keep the game running
+    processInput();
+    update();
+    render();
+    }
+    // not really sure about this part... I googled how to make a game loop...
+    // so I think it might make our game run,
+    // but im not sure how to test it without the other parts of the code
+    // and it seems too simple to me but idk
+
+
+    //print out info about family and individual people
+    System.out.println("Family a has" + length(a) + " people in it");
+    System.out.println("Yours towns population is: " + populationSize);
+
+    }
+
